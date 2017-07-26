8c4eea2fdd036ee8d896880ebb094d77adb063d0
Joe <joe-sawyer32@users.noreply.github.com>
2017-07-26 13:11:34 -0500

    Student class created

---
+++ src/Student.java
@@ -0,0 +1,47 @@
+/**
+ * Created by Joe on 7/26/17.
+ */
+public class Student {
+    // FIELDS
+    private String firstName;
+    private String lastName;
+    private int id;
+    private int grade;
+
+    // CONSTRUCTORS
+    public Student () {}
+
+    // METHODS
+    // getters and setters
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getGrade() {
+        return grade;
+    }
+
+    public void setGrade(int grade) {
+        this.grade = grade;
+    }
+}
